5916e4a0cab2beeb6dc4c09dad460feeb85b8f27
amarkes <tmowna@gmail.com>
2024-10-15 22:32:15 -0300

    fix menu

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,68 +1,9 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { NavLink, Outlet, useLocation } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
 import AuthContext from '@/context/AuthContext';
-import { Button } from '@/components/ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
-import { CgProfile } from "react-icons/cg";
-import { CiSettings } from "react-icons/ci";
-import { FaProductHunt } from "react-icons/fa";
-import { AiOutlineDown } from "react-icons/ai";
-import { Link } from 'react-router-dom';
-
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
-
-// Lista do menu
-const menu = [
-  {
-    name: 'Profile',
-    icon: <CgProfile />,
-    route: '/dashboard/profile'
-  },
-  {
-    name: 'Settings',
-    icon: <CiSettings />,
-    route: '',
-    subMenu: [
-      {
-        name: 'General',
-        icon: <CgProfile />,
-        route: '/dashboard/settings/general'
-      },
-      {
-        name: 'Security',
-        icon: <CgProfile />,
-        route: '/dashboard/settings/security'
-      }
-    ]
-  },
-  {
-    name: 'Products',
-    icon: <FaProductHunt />,
-    route: '',
-    subMenu: [
-      {
-        name: 'Detail',
-        icon: <FaProductHunt />,
-        route: '/dashboard/products/detail'
-      },
-      {
-        name: 'View',
-        icon: <FaProductHunt />,
-        route: '/dashboard/products/view'
-      }
-    ]
-  }
-];
+import Sidebar from '@/components/sidebar/Sidebar';
 
 // Função para mapear a primeira letra para uma cor
 const getAvatarBackgroundColor = (initial) => {
@@ -83,45 +24,12 @@ const getInitials = (name) => {
 
 const HomePage = () => {
   const { user, logout } = useContext(AuthContext);
-  const location = useLocation();
-  const [openSubMenus, setOpenSubMenus] = useState([]); // Lista de submenus abertos
-
   const initials = getInitials(user);
   const avatarBgColor = getAvatarBackgroundColor(initials.charAt(0));
 
-  // Verifica se a rota atual corresponde a um item do submenu e mantém aberto
-  // useEffect(() => {
-  //   setOpenSubMenus((prev) => {
-  //     const newOpenSubMenus = [...prev];
-  //     menu.forEach((menuItem, index) => {
-  //       if (menuItem.subMenu) {
-  //         menuItem.subMenu.forEach((subItem) => {
-  //           if (location.pathname.startsWith(subItem.route) && !newOpenSubMenus.includes(index)) {
-  //             newOpenSubMenus.push(index);
-  //           }
-  //         });
-  //       }
-  //     });
-  //     return newOpenSubMenus;
-  //   });
-  // }, [location]);
-
-  const toggleSubMenu = (menuIndex) => {
-
-    console.log(openSubMenus.includes(menuIndex))
-    if (openSubMenus.includes(menuIndex)) {
-      setOpenSubMenus(openSubMenus.filter((index) => index !== menuIndex)); // Fecha o submenu
-    } else {
-      setOpenSubMenus([...openSubMenus, menuIndex]); // Abre o submenu
-    }
-    // setOpenSubMenus((prev) => {
-    //   if (prev.includes(menuIndex)) {
-    //     return prev.filter((index) => index !== menuIndex); // Fecha o submenu
-    //   } else {
-    //     return [...prev, menuIndex]; // Abre o submenu
-    //   }
-    // });
-  };
+  useEffect(() => {
+    console.log(new Date())
+  });
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col">
@@ -165,87 +73,7 @@ const HomePage = () => {
 
       <div className="flex flex-1">
         {/* Sidebar */}
-        <aside className="w-64 bg-white text-gray-600 flex flex-col">
-          <div className="px-6 py-4">
-            <div className="text-2xl font-bold">Dashboard</div>
-          </div>
-          <nav className="mt-6 flex-1 mx-4">
-            <ul>
-              {menu.map((menuItem, index) => (
-                <li key={index}>
-                  {menuItem.subMenu ? (
-                    <div>
-                      <button
-                        className={`flex justify-between items-center block py-2.5 px-4 rounded-md w-full text-left transition-all hover:bg-gray-100`}
-                        onClick={() => toggleSubMenu(index)}
-                      >
-                        <span className="flex gap-2 items-center">
-                          {menuItem.icon}
-                          {menuItem.name}
-                        </span>
-                        {/* Seta para baixo */}
-                        <AiOutlineDown className={`transition-transform duration-200 ${openSubMenus.includes(index) ? 'rotate-180' : 'rotate-0'}`} />
-                      </button>
-                      <div className={`transition-all duration-200 ease-in-out ${openSubMenus.includes(index) ? 'max-h-screen' : 'max-h-0 overflow-hidden'}`}>
-                        <ul className="pl-4">
-                          {menuItem.subMenu.map((subItem, subIndex) => (
-                            <li key={subIndex}>
-                              <Link to={subItem.route} className="flex gap-2 items-center block py-2.5 px-4 rounded-md hover:bg-gray-100">{subItem.icon}
-                                {subItem.name}</Link>
-                              {/* <NavLink
-                                to={subItem.route}
-                                className={({ isActive }) =>
-                                  isActive
-                                    ? 'flex gap-2 items-center block py-2.5 px-4 rounded-md bg-gray-200 text-gray-900'
-                                    : 'flex gap-2 items-center block py-2.5 px-4 rounded-md hover:bg-gray-100'
-                                }
-                                onClick={(e) => e.stopPropagation()}
-                                replace
-                              >
-                                {subItem.icon}
-                                {subItem.name}
-                              </NavLink> */}
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    </div>
-                  ) : (
-                    <NavLink
-                      to={menuItem.route}
-                      className={({ isActive }) =>
-                        isActive
-                          ? 'flex gap-2 justify-start items-center block py-2.5 px-4 rounded-md bg-gray-200 text-gray-900'
-                          : 'flex gap-2 justify-start items-center block py-2.5 px-4 rounded-md hover:bg-gray-100'
-                      }
-                      replace
-                    >
-                      {menuItem.icon}
-                      {menuItem.name}
-                    </NavLink>
-                  )}
-                </li>
-              ))}
-            </ul>
-            <NavigationMenu orientation="vertical">
-              <NavigationMenuList>
-                <NavigationMenuItem>
-                  <NavigationMenuTrigger>Item one</NavigationMenuTrigger>
-                  <NavigationMenuContent>Item one content</NavigationMenuContent>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationMenuTrigger>Item two</NavigationMenuTrigger>
-                  <NavigationMenuContent>Item Two content</NavigationMenuContent>
-                </NavigationMenuItem>
-              </NavigationMenuList>
-            </NavigationMenu>
-          </nav>
-          <div className="p-4">
-            <Button onClick={logout} className="w-full">
-              Logout
-            </Button>
-          </div>
-        </aside>
+        <Sidebar />
 
         {/* Main content */}
         <div className="flex-1 p-10">
---
+++ src/components/sidebar/SideBar.jsx
@@ -0,0 +1,176 @@
+import React, { useState, useContext, useEffect } from 'react';
+import AuthContext from '@/context/AuthContext';
+import { Link } from 'react-router-dom';
+
+import { CgProfile } from "react-icons/cg";
+import { CiSettings } from "react-icons/ci";
+import { FaProductHunt } from "react-icons/fa";
+import { AiOutlineDown } from "react-icons/ai";
+
+import { Button } from '@/components/ui/button';
+
+import { NavLink, useLocation } from 'react-router-dom';
+// Lista do menu
+const menu = [
+  {
+    name: 'Profile',
+    icon: <CgProfile />,
+    route: '/dashboard/profile'
+  },
+  {
+    name: 'Settings',
+    icon: <CiSettings />,
+    route: '',
+    subMenu: [
+      {
+        name: 'General',
+        icon: <CgProfile />,
+        route: '/dashboard/settings/general'
+      },
+      {
+        name: 'Security',
+        icon: <CgProfile />,
+        route: '/dashboard/settings/security'
+      }
+    ]
+  },
+  {
+    name: 'Products',
+    icon: <FaProductHunt />,
+    route: '',
+    subMenu: [
+      {
+        name: 'Detail',
+        icon: <FaProductHunt />,
+        route: '/dashboard/products/detail'
+      },
+      {
+        name: 'View',
+        icon: <FaProductHunt />,
+        route: '/dashboard/products/view'
+      }
+    ]
+  }
+];
+
+const DashboardMenu = () => {
+  const location = useLocation();
+  const { user, logout } = useContext(AuthContext);
+  const [openSubMenus, setOpenSubMenus] = useState(() => {
+    // Recupera o estado do localStorage
+    const savedState = localStorage.getItem('openSubMenus');
+    return savedState ? JSON.parse(savedState) : [];
+  });
+
+  const toggleSubMenu = (menuIndex) => {
+    setOpenSubMenus((prev) => {
+      const updatedMenus = prev.includes(menuIndex)
+        ? prev.filter((index) => index !== menuIndex) // Fecha o submenu
+        : [...prev, menuIndex]; // Abre o submenu
+
+      // Salva o estado atualizado no localStorage
+      localStorage.setItem('openSubMenus', JSON.stringify(updatedMenus));
+      return updatedMenus;
+    });
+  };
+
+  // Verifica se a rota atual corresponde a um item do submenu e mantém aberto
+  useEffect(() => {
+    setOpenSubMenus((prev) => {
+      const newOpenSubMenus = [...prev];
+      menu.forEach((menuItem, index) => {
+        if (menuItem.subMenu) {
+          menuItem.subMenu.forEach((subItem) => {
+            if (location.pathname.startsWith(subItem.route) && !newOpenSubMenus.includes(index)) {
+              newOpenSubMenus.push(index);
+            }
+          });
+        }
+      });
+      return newOpenSubMenus;
+    });
+  }, [location]);
+
+  const isSubMenuActive = (subMenu) => {
+    return subMenu.some((subItem) => location.pathname.startsWith(subItem.route));
+  };
+
+  return (
+    <aside className="w-64 bg-white text-gray-600 flex flex-col">
+      <div className="px-6 py-4">
+        <div className="text-2xl font-bold">Dashboard</div>
+      </div>
+      <nav className="mt-6 flex-1 mx-4">
+        <ul>
+          {menu.map((menuItem, index) => (
+            <li key={index}>
+              {menuItem.subMenu ? (
+                <div>
+                  <button
+                    className={`${isSubMenuActive(menuItem.subMenu) ? 'bg-gray-100 text-gray-900' : ''} flex justify-between items-center block py-2.5 px-4 rounded-md w-full text-left transition-all hover:bg-gray-100`}
+                    onClick={() => toggleSubMenu(index)}
+                  >
+                    <span className="flex gap-2 items-center">
+                      {menuItem.icon}
+                      {menuItem.name}
+                    </span>
+                    {/* Seta para baixo */}
+                    <AiOutlineDown className={`transition-transform duration-200 ${openSubMenus.includes(index) ? 'rotate-180' : 'rotate-0'}`} />
+                  </button>
+                  <div className={`transition-all duration-200 ease-in-out ${openSubMenus.includes(index) ? 'max-h-screen' : 'max-h-0 overflow-hidden'}`}>
+                    <ul className="pl-4">
+                      {menuItem.subMenu.map((subItem, subIndex) => (
+                        <li key={subIndex}>
+                          {/* <Link to={subItem.route} className={
+                            location.pathname.startsWith(subItem.route)
+                            ? 'flex gap-2 items-center block py-2.5 px-4 rounded-md bg-gray-200 text-gray-900'
+                            : 'flex gap-2 items-center block py-2.5 px-4 rounded-md hover:bg-gray-100'
+                          }>{subItem.icon}
+                                {subItem.name}</Link> */}
+                          <NavLink
+                            to={subItem.route}
+                            className={({ isActive }) =>
+                              isActive
+                                ? 'flex gap-2 items-center block py-2.5 px-4 rounded-md bg-gray-200 text-gray-900'
+                                : 'flex gap-2 items-center block py-2.5 px-4 rounded-md hover:bg-gray-100'
+                            }
+                            onClick={(e) => e.stopPropagation()}
+                            replace
+                          >
+                            {subItem.icon}
+                            {subItem.name}
+                          </NavLink>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              ) : (
+                <NavLink
+                  to={menuItem.route}
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'flex gap-2 justify-start items-center block py-2.5 px-4 rounded-md bg-gray-200 text-gray-900'
+                      : 'flex gap-2 justify-start items-center block py-2.5 px-4 rounded-md hover:bg-gray-100'
+                  }
+                  replace
+                >
+                  {menuItem.icon}
+                  {menuItem.name}
+                </NavLink>
+              )}
+            </li>
+          ))}
+        </ul>
+
+      </nav>
+      <div className="p-4">
+        <Button onClick={logout} className="w-full">
+          Logout
+        </Button>
+      </div>
+    </aside>
+  );
+};
+
+export default DashboardMenu;
